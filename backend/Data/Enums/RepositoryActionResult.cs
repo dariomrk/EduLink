f9878d35bb646ae7ea8b66284b01191b61881af0
Dario <dariomrkonjic1@gmail.com>
2023-05-11 21:11:03 +0200

    Add generic repository implementation to backend Data layer (#11)
    * Add generic Repository interface.
    
    * Implement generic repository.

---
+++ backend/Data/Enums/RepositoryActionResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Data.Enums
+{
+    public enum RepositoryActionResult
+    {
+        Success,
+        NoChanges,
+        Error,
+    }
+}
---
+++ backend/Data/Interfaces/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using Data.Enums;
+using System.Linq.Expressions;
+
+namespace Data.Interfaces
+{
+    public interface IRepository<TModel, TId> where TModel : class, IEduLinkModel where TId : struct
+    {
+        IQueryable<TModel> Query();
+        Task<ICollection<TModel>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<TModel?> FindByIdAsync(TId id, CancellationToken cancellationToken = default);
+        Task<TModel?> FindAsync(Expression<Func<TModel, bool>> selector, CancellationToken cancellationToken = default);
+        Task<ICollection<TModel>> WhereAsync(Expression<Func<TModel, bool>> selector, CancellationToken cancellationToken = default);
+        Task<bool> CheckExistsAsync(Expression<Func<TModel, bool>> selector, CancellationToken cancellationToken = default);
+
+        Task BeginTransactionAsync();
+        Task CommitTransactionAsync();
+        Task RollbackTransactionAsync();
+
+        Task<(RepositoryActionResult Result, TModel? Created)> CreateAsync(TModel model);
+        Task<(RepositoryActionResult Result, TModel? Updated)> UpdateAsync(TModel model);
+        Task<RepositoryActionResult> DeleteAsync(TId id);
+    }
+}
