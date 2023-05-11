345ba2b6a389439a014e73f0de75654e1f2179e1
dariomrk <dariomrkonjic1@gmail.com>
2023-05-11 21:04:06 +0200

    Scaffold UserService.

---
+++ backend/Application/Services/UserService.cs
@@ -0,0 +1,30 @@
+﻿using Application.Dtos.Common;
+using Application.Dtos.User;
+using Application.Enums;
+using Application.Interfaces;
+
+namespace Application.Services
+{
+    public class UserService : IUserService
+    {
+        public Task<UserDto> GetStudentAsync(string myUsername, long id, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<UserDto>> GetStudentsAsync(string myUsername, string? countryName = null, string? regionName = null, string? cityName = null, PaginationDto? paginationOptions = null, SortOrder sortOrder = SortOrder.Ascending, SortProperties orderBy = SortProperties.Rating, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDto> GetTutorAsync(long id, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<UserDto>> GetTutorsAsync(string? countryName = null, string? regionName = null, string? cityName = null, PaginationDto? paginationOptions = null, SortOrder sortOrder = SortOrder.Ascending, SortProperties orderBy = SortProperties.Rating, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
