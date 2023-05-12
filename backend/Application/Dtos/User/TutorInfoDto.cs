4aaacb40f64b373dbfdd592966509c166e0c421e
dariomrk <dariomrkonjic1@gmail.com>
2023-05-12 19:55:59 +0200

    Configure Dtos.

---
+++ backend/Application/Dtos/User/TutorInfoDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Dtos.User
+{
+    public class TutorInfoDto
+    {
+        public double AverageRating { get; set; }
+        public int TotalTutoringHours { get; set; }
+    }
+}
--- backend/Application/Dtos/User/UserDto.cs
+++ backend/Application/Dtos/User/UserDto.cs
@@ -1,4 +1,15 @@
 ﻿namespace Application.Dtos.User
 {
-    public class UserDto { }
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Username { get; set; } = null!;
+        public string About { get; set; } = null!;
+        public string CityName { get; set; } = null!;
+
+        public bool IsTutor => TutorInfo is not null;
+        public TutorInfoDto? TutorInfo { get; set; }
+    }
 }
