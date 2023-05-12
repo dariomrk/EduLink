17bb5fe1d97ba5f4567f713f9831ced812bcb47e
dariomrk <dariomrkonjic1@gmail.com>
2023-05-12 21:50:27 +0200

    Scaffold TutoringPostService.

---
+++ backend/Application/Services/TutoringPostService.cs
@@ -0,0 +1,31 @@
+﻿using Application.Dtos.Common;
+using Application.Dtos.TutoringPost;
+using Application.Enums;
+using Application.Interfaces;
+
+namespace Application.Services
+{
+    public class TutoringPostService : ITutoringPostService
+    {
+        public Task<(ServiceActionResult Result, TutoringPostDto? Created)> CreateTutoringPostAsync(CreateDto post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TutoringPostDto> GetTutoringPostAsync(long id, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TutoringPostDto> GetTutoringPostsAsync(
+            string? countryName = null,
+            string? regionName = null,
+            string? cityName = null,
+            PaginationDto? paginationOptions = null,
+            SortDto? sortOptions = null,
+            CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
