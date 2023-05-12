04cdd9de6c289563898e29992f924a88b6026c89
dariomrk <dariomrkonjic1@gmail.com>
2023-05-12 20:56:25 +0200

    Change access level for internal methods.

--- backend/Application/Mappings/LocationMappings.cs
+++ backend/Application/Mappings/LocationMappings.cs
@@ -5,10 +5,10 @@ using Riok.Mapperly.Abstractions;
 namespace Application.Mappings
 {
     [Mapper]
-    public static partial class LocationMappings
+    internal static partial class LocationMappings
     {
-        public static partial CountryDto ToDto(this Country country);
-        public static partial RegionDto ToDto(this Region region);
-        public static partial CityDto ToDto(this City city);
+        internal static partial CountryDto ToDto(this Country country);
+        internal static partial RegionDto ToDto(this Region region);
+        internal static partial CityDto ToDto(this City city);
     }
 }
