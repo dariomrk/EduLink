ffbf7931ad757c3713c47a67b5f0f66316b95d50
Josipa Šaravanja <josipa.saravanja06@gmail.com>
2023-05-15 15:39:55 +0200

    user controller

---
+++ backend/Api/Controller/UserController.cs
@@ -0,0 +1,110 @@
+﻿using Application.Dtos.Common;
+using Application.Dtos.User;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+
+        [HttpGet(Routes.Users.GetTutorsInCity)]
+        public async Task<ActionResult<UserResponseDto>> GetTutorsInCity(
+           [FromRoute] string countryName,
+           [FromRoute] string regionName,
+           [FromRoute] string cityName,
+           PaginationRequestDto? paginationDto,
+            SortRequestDto? sortOptions,
+            CancellationToken cancellationToken = default)
+        {
+            var result = await _userService.GetTutorsInCityAsync(countryName, regionName, cityName, paginationDto, sortOptions, cancellationToken);
+
+            if (result is null)
+                return NotFound();
+
+            return Ok(result.ToDto());
+        }
+
+        [HttpGet(Routes.Users.GetTutorsInRegion)]
+        public async Task<ActionResult<UserResponseDto>> GetTutorsInRegion(
+           [FromRoute] string countryName,
+           [FromRoute] string regionName,
+           PaginationRequestDto? paginationDto,
+            SortRequestDto? sortOptions,
+            CancellationToken cancellationToken = default)
+        {
+            var result = await _userService.GetTutorsInRegionAsync(countryName, regionName, paginationDto, sortOptions, cancellationToken);
+
+            if (result is null)
+                return NotFound();
+
+            return Ok(result.ToDto());
+        }
+
+        [HttpGet(Routes.Users.GetStudents)]
+        public async Task<ActionResult<UserResponseDto>> GetAllStudents(
+           string tutorUsername, //jwt
+           PaginationRequestDto? paginationDto,
+            SortRequestDto? sortOptions,
+            CancellationToken cancellationToken = default)
+        {
+            var result = await _userService.GetStudentsAsync(tutorUsername, paginationDto, sortOptions, cancellationToken);
+
+            if (result is null)
+                return NotFound();
+
+            return Ok(result.ToDto());
+        }
+
+        [HttpGet(Routes.Users.GetStudents)]
+        public async Task<ActionResult<UserResponseDto>> GetStudents(
+            [FromRoute] string tutorUsername,
+           PaginationRequestDto? paginationDto,
+            SortRequestDto? sortOptions,
+            CancellationToken cancellationToken)
+        {
+            var result = await _userService.GetStudentsAsync(tutorUsername, paginationDto, sortOptions, cancellationToken);
+
+            if (result is null)
+                return NotFound();
+
+            return Ok(result.ToDto());
+        }
+
+        [HttpGet(Routes.Users.GetTutor)]
+        public async Task<ActionResult<UserResponseDto>> FindTutor(
+            [FromRoute] string tutorUsername,
+            CancellationToken cancellationToken)
+        {
+            var result = await _userService.GetTutorAsync(tutorUsername, cancellationToken);
+
+            if (result is null)
+                return NotFound();
+
+            return Ok(result.ToDto());
+        }
+
+        [HttpGet(Routes.Users.GetStudent)]
+        public async Task<ActionResult<UserResponseDto>> FindStudent(
+            string tutorUsername, //jwt
+            [FromRoute] long studentId,
+            CancellationToken cancellationToken)
+        {
+            var result = await _userService.GetStudentAsync(tutorUsername, studentId, cancellationToken);
+
+            if (result is null)
+                return NotFound();
+
+            return Ok(result.ToDto());
+        }        
+    }
+}
---
+++ backend/Api/Routes.cs
@@ -0,0 +1,42 @@
+﻿namespace Api
+{
+    public class Routes
+    {
+        private const string Base = "api";
+
+        public static class Users
+        {
+            public const string GetTutorsInCity = Base + $"/users/tutors/{{countryName:string}}/region/{{regionName:string}}/city/{{cityName:string}}";
+            public const string GetTutorsInRegion = Base + $"/users/tutors/{{countryName:string}}/region/{{regionName:string}}";
+            public const string GetAllStudents = Base + $"/users/students";
+            public const string GetStudent = Base + $"/users/students/{{username:string}}";
+            public const string GetTutor = Base + $"/users/tutors/{{username:string}}";
+        }
+            public static class Posts
+        {
+            public const string GetAll = Base + $"/users/country/{{countryName:string}}/region/{{regionName:string}}/city/{{cityName:string}}/posts";
+            public const string Get = Base + $"/posts /{{id:int}}";
+            public const string Create = Base + $"/posts ";
+        }
+
+        public static class Appointments
+        {
+            public const string GetAll = Base + $"/appointments";
+            public const string Get = Base + $"/appointments/{{id:int}}";
+            public const string Create = Base + $"/appointments";
+            public const string Cancel = Base + $"/appointments/{{id:int}}/cancel";
+            public const string Review = Base + $"/appointments/{{id:int}}/review";
+        }
+        public static class Messages
+        {
+            public const string GetAll = Base + $"/messages";
+            public const string Get = Base + $"/messages/{{id:int}}";
+            public const string Create = Base + $"/messages/{{id:int}}/new";
+        }
+        public static class Identity
+        {
+            public const string Register = Base + $"/identity/register";
+            public const string Login = Base + $"/identity/login";
+        }
+    }
+}
