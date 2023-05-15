aadfae351f772c0b248b371399a6a6a12f118317
dariomrk <dariomrkonjic1@gmail.com>
2023-05-15 14:24:33 +0200

    Scaffold MessageService.

---
+++ backend/Application/Services/MessageService.cs
@@ -0,0 +1,34 @@
+﻿using Application.Dtos.Common;
+using Application.Dtos.Message;
+using Application.Enums;
+using Application.Interfaces;
+
+namespace Application.Services
+{
+    public class MessageService : IMessageService
+    {
+        public Task<ICollection<MessageResponseDto>> GetMessagesAsync(
+            string recipientUsername,
+            string senderUsername,
+            PaginationRequestDto? paginationDto = null,
+            SortRequestDto? sortOptions = null,
+            CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<MessageResponseDto>> GetMessagesAsync(
+            string username,
+            PaginationRequestDto? paginationDto = null,
+            SortRequestDto? sortOptions = null,
+            CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<(ServiceActionResult Result, MessageResponseDto? Created)> SendMessage(MessageRequestDto message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
