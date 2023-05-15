ac0e22c6dcc068d966d50f661bf2d5eba68336a3
dariomrk <dariomrkonjic1@gmail.com>
2023-05-15 18:26:47 +0200

    Implement FieldService.

--- backend/Application/Interfaces/IFieldService.cs
+++ backend/Application/Interfaces/IFieldService.cs
@@ -4,7 +4,7 @@
     {
         internal Task<bool> SubjectContainsField(string subjectName, string fieldName, CancellationToken cancellationToken = default);
         internal Task<bool> SubjectExists(string subjectName, CancellationToken cancellationToken = default);
-        internal Task<(int SubjectKey, ICollection<int> FieldKeys)> GetSubjectAndFieldKeys(
+        internal Task<(long SubjectKey, ICollection<long> FieldKeys)> GetSubjectAndFieldKeys(
             string subjectName,
             ICollection<string> fieldName,
             CancellationToken cancellationToken = default);
