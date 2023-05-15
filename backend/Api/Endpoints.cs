06926871600a6e395ab19d9750f2940f5fcdcc88
dariomrk <dariomrkonjic1@gmail.com>
2023-05-15 20:28:04 +0200

    Add Endpoint.cs
    - Define Users routes

---
+++ backend/Api/Endpoints.cs
@@ -0,0 +1,17 @@
+﻿namespace Api
+{
+    public static class Endpoints
+    {
+        private const string ApiBase = "api";
+
+        public static class Users
+        {
+            private const string ControllerBase = $"{ApiBase}/users";
+
+            public const string GetAllTutors = $"{ControllerBase}/tutors/{{countryName}}/{{regionName}}/{{cityName}}";
+            public const string GetAllStudents = $"{ControllerBase}/students";
+            public const string GetStudent = $"{ControllerBase}/students/{{id:int}}";
+            public const string GetTutor = $"{ControllerBase}/tutors/{{id:int}}";
+        }
+    }
+}
