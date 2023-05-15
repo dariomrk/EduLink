29b0077901c1969486d546d4734400e103919b3a
Josipa Šaravanja <josipa.saravanja06@gmail.com>
2023-05-15 18:22:19 +0200

    formated

--- frontend/src/components/CustomButton/CustomButton.jsx
+++ frontend/src/components/CustomButton/CustomButton.jsx
@@ -1,14 +1,10 @@
-import { Link } from 'react-router-dom';
-import { Color } from '../../style/colors.js';
-import { Button } from '@mantine/core';
+import { Button } from '@mantine/core'
+import React from 'react'
 
 export const CustomButton = (props) => {
-    console.log(Color.Primary)
-    return (
-        /*         <Link to={props.link}>*/
-        <Button color="purple" variant={props.variant} style={{ maxWidth: "100%", height: 'auto', fontSize: "16px", fontWeight: 700, padding: "16px 0", width: "320px", overflowWrap: 'break-word' }} radius="10px">
-            {props.value}
+  return (
+        <Button color="purple" variant={props.variant} style={{ maxWidth: '100%', height: 'auto', fontSize: '16px', fontWeight: 700, padding: '16px 0', width: '320px', overflowWrap: 'break-word' }} radius="10px">
+            {props.text}
         </Button>
-        /*         </Link >*/
-    )
+  )
 }
