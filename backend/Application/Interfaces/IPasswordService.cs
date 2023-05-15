e45bfef5da5f4ae1a24d66957bb0bc0a5ae79162
dariomrk <dariomrkonjic1@gmail.com>
2023-05-15 12:13:35 +0200

    Implement PasswordService.

---
+++ backend/Application/Interfaces/IPasswordService.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Interfaces
+{
+    public interface IPasswordService
+    {
+        internal (byte[] PasswordHash, byte[] Salt) GeneratePassword(string password, int iterations, int hashLength, int saltLength);
+    }
+}
---
+++ backend/Application/Services/PasswordService.cs
@@ -0,0 +1,19 @@
+﻿using Application.Interfaces;
+using System.Security.Cryptography;
+
+namespace Application.Services
+{
+    public class PasswordService : IPasswordService
+    {
+        public (byte[] PasswordHash, byte[] Salt) GeneratePassword(string password, int iterations, int hashLength, int saltLength)
+        {
+            var salt = RandomNumberGenerator.GetBytes(saltLength);
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations, HashAlgorithmName.SHA512);
+            var hash = pbkdf2.GetBytes(hashLength);
+
+            pbkdf2.Dispose();
+
+            return (hash, salt);
+        }
+    }
+}
