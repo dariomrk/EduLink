0549ff896e748d363bacedd09c4660ed6e105c82
dariomrk <dariomrkonjic1@gmail.com>
2023-05-15 23:56:45 +0200

    Adjust database.

---
+++ backend/Data/Migrations/20230515213459_RemoveRequiredImageOnTableUser.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveRequiredImageOnTableUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_User_File_ProfileImageId",
+                table: "User");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "ProfileImageId",
+                table: "User",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_User_File_ProfileImageId",
+                table: "User",
+                column: "ProfileImageId",
+                principalTable: "File",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_User_File_ProfileImageId",
+                table: "User");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "ProfileImageId",
+                table: "User",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_User_File_ProfileImageId",
+                table: "User",
+                column: "ProfileImageId",
+                principalTable: "File",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
