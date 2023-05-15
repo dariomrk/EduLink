bff9ef1b6b25a33b8b86177b5e81a3dc230cae41
Josipa Šaravanja <josipa.saravanja06@gmail.com>
2023-05-15 17:59:09 +0200

    added button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import './index.css'
 import { MantineProvider } from '@mantine/core'
 import { Color } from './style/colors.js'
-import CustomButton from './components/CustomButton/index.js'
 
 function App() {
   return (
@@ -13,7 +12,7 @@ function App() {
         },
       }}
     >
-      <CustomButton value="Vrati me na početnu" variant="filled" link="/" />
+
     </MantineProvider>
   )
 }
