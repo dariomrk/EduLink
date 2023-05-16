37490b544b5ce732c29ec8b7de0be1f4b24859e8
Josipa Saravanja <josipa.saravanja06@gmail.com>
2023-05-16 15:44:13 +0200

    removed unnecessary

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,27 @@ import React from 'react'
 import './index.css'
 import { MantineProvider } from '@mantine/core'
 import { Color } from './style/colors.js'
-import { InstructorCard } from './components/InstructorCard/InstructorCard.jsx'
+
 function App () {
   return (
     <MantineProvider
       theme={{
         colors: {
-          purple: [Color.Primary, Color.Secondary, Color.Primary, Color.Primary, Color.Primary, Color.Primary, Color.Primary, Color.Primary, Color.Primary, Color.Primary] // more shades
+          purple: [
+            Color.Primary,
+            Color.Secondary,
+            Color.Primary,
+            Color.Primary,
+            Color.Primary,
+            Color.Primary,
+            Color.Primary,
+            Color.Primary,
+            Color.Primary,
+            Color.Primary
+          ] // more shades
         }
       }}
-    >
-      <InstructorCard name="Nikola" stars="4.5" review="5" distance="1.2 km" />
-    </MantineProvider>
+    ></MantineProvider>
   )
 }
 
