72728d2ecc6b90c1bbfa69dc09af7b37b50a2695
dariomrk <dariomrkonjic1@gmail.com>
2023-05-16 13:47:08 +0200

    Add CustomExceptionHandler middleware.

--- backend/Application/Exceptions/IdentityException.cs
+++ backend/Application/Exceptions/IdentityException.cs
@@ -1,6 +1,6 @@
 ﻿namespace Application.Exceptions
 {
-    internal class IdentityException : InvalidOperationException
+    public class IdentityException : InvalidOperationException
     {
         internal IdentityException(string identity, string request)
             : base($"Request `{request}` cannot be performed by the entity `{identity}`.")
