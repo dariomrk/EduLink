33783139a12c469c0b79ad14f1709d65942cfbe7
Josipa Saravanja <josipa.saravanja06@gmail.com>
2023-05-17 16:40:47 +0200

    add some changes

--- frontend/src/components/CustomButton/CustomButton.jsx
+++ frontend/src/components/CustomButton/CustomButton.jsx
@@ -1,21 +1,24 @@
 import { Button } from '@mantine/core'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const CustomButton = props => {
   return (
-    <Button
-      variant={props.variant}
-      style={{
-        width: props.width,
-        height: 'auto',
-        fontSize: '16px',
-        fontWeight: 700,
-        padding: '16px 0',
-        overflowWrap: 'break-word'
-      }}
-      radius="10px"
-    >
-      {props.text}
-    </Button>
+    <Link to={props.link} style={{ width: '100%' }}>
+      <Button
+        variant={props.variant}
+        style={{
+          width: props.width,
+          height: 'auto',
+          fontSize: '16px',
+          fontWeight: 700,
+          padding: '16px 0',
+          overflowWrap: 'break-word'
+        }}
+        radius="10px"
+      >
+        {props.text}
+      </Button>
+    </Link>
   )
 }
