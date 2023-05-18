ad6bcd8b599a064afbb29bd80e47db6ab91f9d21
Josipa Saravanja <josipa.saravanja06@gmail.com>
2023-05-18 13:48:21 +0200

    add titles on login and register page

--- frontend/src/pages/LogInPage/LogInPage.jsx
+++ frontend/src/pages/LogInPage/LogInPage.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { TextInput, Flex, PasswordInput } from '@mantine/core'
 import CustomButton from '../../components/CustomButton'
 import { Color } from '../../style/colors'
+import { PageTitle } from '../../components/PageTitle/PageTitle'
 
 export const LogInPage = () => {
   const [username, setUsername] = useState('')
@@ -11,58 +12,61 @@ export const LogInPage = () => {
 
   console.log(username, email)
   return (
-    <Flex
-      direction="column"
-      gap="24px"
-      style={{ boxSizing: 'border-box' }}
-      m="auto"
-      p="40px"
-      stayle={{ gap: '24px' }}
-      w={{ base: '330px', sm: '450px', md: '600px', lg: '750px ' }}
-    >
-      <TextInput
-        label="Email/Korisnično ime"
-        value={username || email}
-        onChange={event =>
-          event.currentTarget.value.includes('@')
-            ? (setEmail(event.currentTarget.value), setUsername(''))
-            : (setUsername(event.currentTarget.value), setEmail(''))
-        }
-      />
-
-      <PasswordInput
-        value={password}
-        label="Lozinka"
-        onChange={event => setPassword(event.currentTarget.value)}
-      />
-
-      <Link
-        to="/register"
-        style={{
-          textDecoration: 'underline',
-          textAlign: 'right',
-          color: Color.Button
-        }}
+    <>
+      <PageTitle p="84px 10px" title="Prijava" />
+      <Flex
+        direction="column"
+        gap="24px"
+        style={{ boxSizing: 'border-box' }}
+        m="auto"
+        p="40px"
+        stayle={{ gap: '24px' }}
+        w={{ base: '330px', sm: '450px', md: '600px', lg: '750px ' }}
       >
-        Ne sjećaš se lozinke?
-      </Link>
+        <TextInput
+          label="Email/Korisnično ime"
+          value={username || email}
+          onChange={event =>
+            event.currentTarget.value.includes('@')
+              ? (setEmail(event.currentTarget.value), setUsername(''))
+              : (setUsername(event.currentTarget.value), setEmail(''))
+          }
+        />
 
-      <CustomButton
-        text="Prijavi se"
-        // onClick={() => }
-        width="100%"
-      />
-      <p
-        style={{
-          textAlign: 'center',
-          color: Color.Button
-        }}
-      >
-        Imaš račun?
-        <Link to="/register" style={{ textDecoration: 'underline' }}>
-          Registriraj se
+        <PasswordInput
+          value={password}
+          label="Lozinka"
+          onChange={event => setPassword(event.currentTarget.value)}
+        />
+
+        <Link
+          to="/help"
+          style={{
+            textDecoration: 'underline',
+            textAlign: 'right',
+            color: Color.Button
+          }}
+        >
+          Ne sjećaš se lozinke?
         </Link>
-      </p>
-    </Flex>
+
+        <CustomButton
+          text="Prijavi se"
+          // onClick={() => }
+          width="100%"
+        />
+        <p
+          style={{
+            textAlign: 'center',
+            color: Color.Button
+          }}
+        >
+          Imaš račun?
+          <Link to="/register" style={{ textDecoration: 'underline' }}>
+            Registriraj se
+          </Link>
+        </p>
+      </Flex>
+    </>
   )
 }
--- frontend/src/pages/RegisterPage/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage/RegisterPage.jsx
@@ -5,6 +5,7 @@ import { TextInput, Flex, PasswordInput } from '@mantine/core'
 import CustomButton from '../../components/CustomButton'
 import { Color } from '../../style/colors'
 import { ReactComponent as Calender } from '../../img/calender.svg'
+import PageTitle from '../../components/PageTitle'
 
 export const RegisterPage = () => {
   const [activeStep, setActiveStep] = useState(0)
@@ -105,30 +106,33 @@ export const RegisterPage = () => {
   }
 
   return (
-    <div>
-      <Flex // TODO: Change colors for input outline and labels
-        direction="column"
-        gap="24px"
-        style={{ boxSizing: 'border-box' }}
-        m="auto"
-        p="40px"
-        stayle={{ gap: '24px' }}
-        w={{ base: '330px', sm: '450px', md: '600px', lg: '750px ' }}
-      >
-        {activeStep === 0 ? firstStep() : secondStep()}
-
-        <p
-          style={{
-            textAlign: 'center',
-            color: Color.Button
-          }}
+    <>
+      <PageTitle p="84px 10px" title="Registracija" />
+      <div>
+        <Flex // TODO: Change colors for input outline and labels
+          direction="column"
+          gap="24px"
+          style={{ boxSizing: 'border-box' }}
+          m="auto"
+          p="40px"
+          stayle={{ gap: '24px' }}
+          w={{ base: '330px', sm: '450px', md: '600px', lg: '750px ' }}
         >
-          Imaš račun?
-          <Link to="/login" style={{ textDecoration: 'underline' }}>
-            Prijavi se
-          </Link>
-        </p>
-      </Flex>
-    </div>
+          {activeStep === 0 ? firstStep() : secondStep()}
+
+          <p
+            style={{
+              textAlign: 'center',
+              color: Color.Button
+            }}
+          >
+            Imaš račun?
+            <Link to="/login" style={{ textDecoration: 'underline' }}>
+              Prijavi se
+            </Link>
+          </p>
+        </Flex>
+      </div>
+    </>
   )
 }
