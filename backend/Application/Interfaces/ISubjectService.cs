caabea5c169c9df6c4c65fe0dbddcd87f0df3ce3
Dario <dariomrkonjic1@gmail.com>
2023-05-19 11:58:02 +0200

    Add subjects controller. (#121)

---
+++ backend/Application/Interfaces/ISubjectService.cs
@@ -0,0 +1,10 @@
+﻿using Application.Dtos.Subject;
+
+namespace Application.Interfaces
+{
+    public interface ISubjectService
+    {
+        public Task<ICollection<string>> GetAllSubjectsAsync(CancellationToken cancellationToken = default);
+        public Task<SubjectResponseDto> GetSubjectAsync(string subject, CancellationToken cancellationToken = default);
+    }
+}
